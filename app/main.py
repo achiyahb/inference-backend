3a5f6af524635eab75c35fb2aa9a1f0204b7da92
Achiya Haviv <ahiya.haviv@bewith.io>
2024-06-20 22:55:02 +0300

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.classifier import router as classifier_router
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Classifier API", version="1.0")
+
+    origins = [
+        "http://localhost",  # Front-end running on localhost
+        "http://localhost:3002",  # Front-end running on a different port
+        "http://127.0.0.1:3002",  # Another common localhost setup
+        # Add other origins if needed
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )   
+
     app.include_router(classifier_router)
     return app
 
