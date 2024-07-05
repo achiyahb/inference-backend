ba20f6977c69b653187ce40c8a554982cf4b8efd
Achiya Haviv <ahiya.haviv@bewith.io>
2024-07-05 19:00:51 +0300

    add torch to requirement and import only required

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from fastai.vision.all import load_learner, PILImage
 from io import BytesIO
-import torch
+from torch import device, load
 
 class ClassifierModel:
     def __init__(self, model_stream: BytesIO):
@@ -9,7 +9,7 @@ class ClassifierModel:
     @staticmethod
     def load_model_from_stream(model_stream: BytesIO):
         model_stream.seek(0)  # Ensure the stream is at the beginning
-        return torch.load(model_stream, map_location=torch.device('cpu'))
+        return load(model_stream, map_location=device('cpu'))
 
     def predict(self, image_path: str) -> str:
         image = PILImage.create(image_path)
