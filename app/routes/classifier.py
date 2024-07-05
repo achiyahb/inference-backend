022b1cea97026c93f1f2431e67585446a8b89390
Achiya Haviv <ahiya.haviv@bewith.io>
2024-07-05 08:08:15 +0300

    change s3 path

--- app/routes/classifier.py
+++ app/routes/classifier.py
@@ -9,7 +9,7 @@ from io import BytesIO
 router = APIRouter()
 
 @router.post("/classify", response_model=PredictionResponse)
-async def classify(file: UploadFile = File(...), modelId: int = Form(...)) -> PredictionResponse:
+async def classify(file: UploadFile = File(...), modelKey: str = Form(...), trainerId: int = Form(...)) -> PredictionResponse:
     try:
         if not file.content_type.startswith('image/'):
             raise HTTPException(status_code=400, detail="File provided is not an image.")
@@ -18,7 +18,7 @@ async def classify(file: UploadFile = File(...), modelId: int = Form(...)) -> Pr
         with open(temp_file_path, "wb+") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-        model_bytes = get_model_from_cache_or_storage(modelId)
+        model_bytes = get_model_from_cache_or_storage(modelKey, trainerId)
         classifier_model = ClassifierModel(model_bytes)
 
         prediction = classifier_model.predict(temp_file_path)
--- app/routes/storage.py
+++ app/routes/storage.py
@@ -4,16 +4,16 @@ from ..utils.s3 import download_file
 
 model_cache = {}
 
-def get_model_from_cache_or_storage(model_id: str):
-    if model_id in model_cache:
-        return model_cache[model_id]
+def get_model_from_cache_or_storage(model_key: str, trainer_id: int):
+    if model_key in model_cache:
+        return model_cache[model_key]
     
-    model_file_key = f"models/{model_id}.pkl"
+    model_file_key = f"models/{trainer_id}/{model_key}.pkl"
     model_file_stream = BytesIO()
     try:
         download_file(model_file_key, model_file_stream)
         model_file_stream.seek(0)
-        model_cache[model_id] = model_file_stream
+        model_cache[model_key] = model_file_stream
         return model_file_stream
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error downloading model: {str(e)}")
